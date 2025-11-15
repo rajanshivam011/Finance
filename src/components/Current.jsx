823f081ad4629688cd802c1e479d7345053f6832
Shivam Rajan <shivamrajan217@gmail.com>
2025-11-15 17:07:19 +0530

    Update Current.jsx

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -5,6 +5,12 @@ import Search from './Search';
 
 const articles = [
   {
+  title: 'Exploring the Evolution of FinTech and Technology Acceptance: Bibliometric Analysis of Research Trends and Future Gaps',
+  authors: 'Dr. Rachna Jain, Dr. Shikha Sharma',
+  pages: '1-23',
+  pdfUrl: '/vol-14/10.pdf',
+},
+  {
     title:
       'Agentic AI for Autonomous CI/CD: Towards Self-Adaptive Financial Infrastructure Pipelines',
     authors: '1st Avinash Reddy Segireddy',
